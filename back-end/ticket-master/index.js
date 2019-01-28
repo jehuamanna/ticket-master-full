991fec4980f19326df21e65d8ed44b4f4c66c135
Jehu Shalom <jehuamanna@gmail.com>
2019-01-28 19:28:24 +0530

    added configuration

--- back-end/ticket-master/index.js
+++ back-end/ticket-master/index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const port = 3001
+// const port = 3001
+const PORT = process.env.PORT || 5000
 const app = express()
 var cors = require('cors')
 app.use(cors())
 app.use(express.json())
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/ticket-master')
+CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:3001/ticket-master>' 
+mongoose.connect(CONNECTION_URI)
 .then((result) => console.log('Connected to db'))
 .catch((err) => console.log('error in connection', err))
 
